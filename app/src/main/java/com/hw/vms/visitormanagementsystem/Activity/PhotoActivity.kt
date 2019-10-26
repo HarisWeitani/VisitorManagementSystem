3def31ea0e6ed58acb14f5bb3d596473c0728699
sirwarfox <hweitani@gmail.com>
2019-10-26 14:53:05 +0700

    Add Photo Activity

--- app/src/main/java/com/hw/vms/visitormanagementsystem/Activity/PhotoActivity.kt
+++ app/src/main/java/com/hw/vms/visitormanagementsystem/Activity/PhotoActivity.kt
@@ -3,7 +3,6 @@ package com.hw.vms.visitormanagementsystem.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,54 +14,30 @@ import org.jetbrains.anko.startActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
-class HostActivity : AppCompatActivity() {
+class PhotoActivity : AppCompatActivity() {
 
     lateinit var iv_logo : ImageView
     lateinit var tv_visitor_number : TextView
     lateinit var tv_date_now : TextView
     lateinit var btn_back : Button
     lateinit var btn_next : Button
-    lateinit var actv_host : AutoCompleteTextView
-
-    var hostId : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_host)
-        initHeader()
+        setContentView(R.layout.activity_photo)
         initView()
     }
 
-    private fun initView(){
+    private fun initView() {
         btn_next = findViewById(R.id.btn_next)
         btn_back = findViewById(R.id.btn_back)
-        actv_host = findViewById(R.id.actv_host)
 
         btn_next.setOnClickListener {
-            startActivity<PhotoActivity>()
+            startActivity<HostActivity>()
         }
         btn_back.setOnClickListener {
             super.onBackPressed()
         }
-
-        actv_host.isEnabled = DAO.responseGetHost != null
-
-        actv_host.setOnItemClickListener { parent, view, position, id ->
-            Log.d("ahsiap","asdas $id")
-            hostId = id.toString()
-        }
-        initAutoCompleteHost()
-    }
-    private fun initAutoCompleteHost(){
-        try {
-            actv_host.setAdapter(
-                HostAdapter(
-                    this, R.layout.my_list_item_1,
-                    DAO.responseGetHost?.data as List<DataHost>
-                )
-            )
-            actv_host.isEnabled = DAO.responseGetHost != null
-        }catch (e:Exception){}
     }
 
     private fun initHeader(){
